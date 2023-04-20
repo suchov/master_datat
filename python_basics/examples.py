354edd608556a763b181f823ab60f14c84d8da4f
Artem Sychov <[artem.suchov@gmail.com]>
2023-04-20 15:31:06 +0200

    add some code examples that I can use for the future

---
+++ python_basics/examples.py
@@ -0,0 +1,11 @@
+# great example of looping through the list:
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for n in range(len(fruits)):
+    print(n, fruits[n])
+
+# simple iteration:
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for fruit in fruits:
+    print(fruit)
+
+# You know - programming is actually like learning a new language - it's true!
