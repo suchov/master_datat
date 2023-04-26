7c77162b8058a47863ad76a4dc32c01030f1038d
Artem Sychov <artem@sychov.dev>
2023-04-26 16:42:45 -0500

    a bit of a progress

--- python_basics/course2/README.md
+++ python_basics/course2/README.md
@@ -3,3 +3,6 @@
 https://www.coursera.org/learn/python-functions-files-dictionaries/lecture/zXDg6/reading-a-file
 
 The number of characters in a file count: 
+https://fopp.umsi.education/books/published/fopp/Files/Iteratingoverlinesinafile.html
+
+
--- python_basics/course2/num_char.py
+++ python_basics/course2/num_char.py
@@ -6,3 +6,30 @@ for string in fileref:
     num_char += len(string)
 fileref.close()
 
+# count the number of lines in file
+fileref = open("travel_plans2.txt", "r")
+## other code here that refers to variable fileref
+file_lines = fileref.readlines()
+num_lines = 0
+for string in fileref:
+    num_lines += 1
+fileref.close()
+
+# read through the file and print the propper programmer way
+olypmicsfile = open("olypmics.txt", "r")
+
+for aline in olypmicsfile:
+    values = aline.split(",")
+    print(values[0], "is from", values[3], "and is on the roster for", values[4])
+
+olypmicsfile.close()
+
+# count the number of lines in a file 
+olypmicsfile = open("emotion_words.txt", "r")
+
+num_lines = 0
+for aline in olypmicsfile:
+    num_lines += 1
+    
+olypmicsfile.close()
+
