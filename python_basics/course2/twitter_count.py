9f93d6bb2d814ec7ee0830b79081f1ff9d0cbd37
Artem Sychov <artem@sychov.dev>
2023-05-27 23:42:08 +0200

    write some comments to read and write methods

--- python_basics/course2/twitter_count.py
+++ python_basics/course2/twitter_count.py
@@ -42,21 +42,24 @@ def get_neg(sentence):
             negative_count = negative_count + 1
     return negative_count
 
-# open the file that contains some what???
+# open the files and write for read and write 
 projectTwitterDataFile = open("project_twitter_data.csv","r")
 resultingDataFile = open("resulting_data.csv","w")
 def writeInDataFile(resultingDataFile):
+    ''' method reading and writing to the file result data'''
+    # write the header
     resultingDataFile.write("Number of Retweets, Number of Replies, Positive Score, Negative Score, Net Score")
     resultingDataFile.write("\n")
 
+    # read the twitter data file in lines
     linesPTDF =  projectTwitterDataFile.readlines()
+    # remove header
     headerDontUsed= linesPTDF.pop(0)
+    # for each line it the file write it down in the right format  
     for linesTD in linesPTDF:
         listTD = linesTD.strip().split(',')
         resultingDataFile.write("{}, {}, {}, {}, {}".format(listTD[1], listTD[2], get_pos(listTD[0]), get_neg(listTD[0]), (get_pos(listTD[0])-get_neg(listTD[0]))))    
         resultingDataFile.write("\n")
-
-        
 
 writeInDataFile(resultingDataFile)
 projectTwitterDataFile.close()
